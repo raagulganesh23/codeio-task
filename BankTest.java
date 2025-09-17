06ca4ee061e54e3ea3367c4001ca3a07c8dbce17
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 07:40:08 +0530

    v0

---
+++ BankTest.java
@@ -0,0 +1,16 @@
+public class BankTest {
+    public static void main(String[] args){
+        int balance = -120;
+        if (balance>0){
+            System.out.println("Postive Balance");
+        } else if (balance<0) {
+            System.out.println("Overdraft");
+        }
+        else {
+            System.out.println("Zero Balance");
+        }
+        {
+
+        }
+    }
+}
