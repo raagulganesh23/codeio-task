e26ea296afbef46b3dd518c46acba5c9a096e969
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 18:06:40 +0530

    v0

---
+++ ExamResult.java
@@ -0,0 +1,11 @@
+public class ExamResult {
+    public static void main(String[] args){
+        int studentScore = 51;
+        if (studentScore>=50){
+            System.out.println("Pass");
+        }
+        else {
+            System.out.println("Fail");
+        }
+    }
+}
