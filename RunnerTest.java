d7312900bf6f17c22da526bc8a4539234899e63d
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 07:50:32 +0530

    v0

---
+++ RunnerTest.java
@@ -0,0 +1,15 @@
+public class RunnerTest {
+    public static void main(String[] args){
+        int time1 = 18;
+        int time2 = 18;
+        if (time1<time2){
+            System.out.println("Arjun");
+        }
+        else if(time1>time2){
+            System.out.println("Varun");
+        }
+        else {
+            System.out.print("Tie");
+        }
+    }
+}
