43e1348cc2107ee3f831f1f92248673a710c00b9
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 07:25:02 +0530

    v0

---
+++ BulbTest.java
@@ -0,0 +1,11 @@
+public class BulbTest {
+    public static void main(String[] args){
+        int bulb = 4;
+        if (bulb%2==0) {
+            System.out.println("ON");
+        }
+        else{
+            System.out.println("OFF");
+        }
+    }
+}
