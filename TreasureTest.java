23fc14c2ffe11a948add692983bce90928c475bf
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 08:19:22 +0530

    v0

---
+++ TreasureTest.java
@@ -0,0 +1,24 @@
+public class TreasureTest {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 50;
+        int c = 75;
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a);
+            } else {
+                System.out.println(c);
+            }
+        } else {
+            if (b>c){
+                System.out.println(b);
+            }
+            else {
+                System.out.println(c);
+            }
+
+        }
+
+    }
+}
