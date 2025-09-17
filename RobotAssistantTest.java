a997958bd95a9633957cd842803c4e7603dbc5db
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 17:56:00 +0530

    v0

---
+++ RobotAssistantTest.java
@@ -0,0 +1,20 @@
+public class RobotAssistantTest {
+    public static void main(String[] args){
+        int a = 6; int b =7;
+        int op = 3;
+        if (op==1) {
+            System.out.println(a+b);
+        }
+        else if (op==2){
+            System.out.println(a-b);
+        }
+        else if (op==3){
+            System.out.println(a*b);
+        }
+        else if (op==4){
+            System.out.println(a/b);
+        }
+    }
+
+}
+
