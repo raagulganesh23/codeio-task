808b012a5f0feafbb6f4077de9b82040ee90b9cb
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 18:03:04 +0530

    v0

---
+++ SpeedCheck.java
@@ -0,0 +1,13 @@
+public class SpeedCheck {
+    public static void main(String[] args){
+        int speedLimit = 60; //Units km/hr
+        int carSpeed = 61; //Units km/hr
+        if (carSpeed>speedLimit){
+            System.out.println("Overspeeding! Pay Fine");
+        }
+        else {
+            System.out.println("You are within the speed limit");
+        }
+
+    }
+}
