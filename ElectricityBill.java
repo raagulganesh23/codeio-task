3ba32f087dfb8223f9def398efc3eb450003a3a1
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-17 18:38:38 +0530

    v0

---
+++ ElectricityBill.java
@@ -0,0 +1,20 @@
+public class ElectricityBill {
+    public static void main(String[] args){
+        int familyBill = 550;//Units - Rupees
+        float x;
+        if (familyBill > 1000){
+            x= 10/100f;
+            float finalBill = familyBill*x;
+            System.out.println(familyBill-finalBill);
+        }
+        else if (familyBill>=500&&familyBill<=1000){
+            x = 5/100f;
+            float finalBill = familyBill*x;
+            System.out.println(familyBill-finalBill);
+        }
+        else{
+            System.out.println(familyBill);
+        }
+
+    }
+}
