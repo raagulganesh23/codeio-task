b7ed323b0c9c8aa0d8a634f040f2b85b37c87ac2
raagulganesh23 <raagulganesh23@gmail.com>
2025-09-24 19:03:30 +0530

    v0

--- Week2/Count.java
+++ Week2/Count.java
@@ -1,4 +1,27 @@
 package Week2;
 
+import java.util.zip.ZipEntry;
+
 public class Count {
+    public static void main(String[] args){
+        int [] arr = {5, -3, 0, 12, -9, 0, 7};
+        int positiveValue=0;
+        int negativeValue=0;
+        int zeroValue=0;
+        for (int i:arr){
+            if (i>0){
+                positiveValue=positiveValue+1;
+            } else if (i<0) {
+                negativeValue=negativeValue+1;
+            }
+            else{
+                zeroValue=zeroValue+1;
+            }
+
+        }
+        System.out.println("No of Postive values = "+positiveValue);
+        System.out.println("No of Negative values = "+negativeValue);
+        System.out.println("No of Zero values = "+ zeroValue);
+    }
+
 }
---
+++ Week2/EvenIndex.java
@@ -0,0 +1,14 @@
+package Week2;
+
+public class EvenIndex {
+    public static void main(String[] args){
+        int [] arr = {10, 20, 30, 40, 50, 60};
+        for (int i =0; i<arr.length; ){
+            System.out.print(arr[i]+" ");
+            i=i+2;
+
+        }
+    }
+
+
+}
